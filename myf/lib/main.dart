3c720096c617391b2053c6085d6c0aeacc08499f
kumanduo <kumanduo@qq.com>
2020-06-03 08:35:16 +0800

    save change

--- myf/lib/main.dart
+++ myf/lib/main.dart
@@ -2,25 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:myflutter/routes.dart';
 import './pages/welcome.dart';
 
-
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //showSemanticsDebugger:true,
-      routes: staticRoutes,
-      home: Myhome()
-    );
+        //showSemanticsDebugger:true,
+        routes: staticRoutes,
+        home: Myhome());
   }
 }
 
 class Myhome extends StatelessWidget {
- 
- 
-
   @override
   Widget build(BuildContext context) {
     return Welcomepage();
--- myf/lib/posts.dart
+++ myf/lib/posts.dart
@@ -1,16 +1,15 @@
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class Post{
-  final String title,author,imageUrl;
+class Post {
+  final String title, author, imageUrl;
 
   Post(this.title, this.author, this.imageUrl);
-
 }
 
 List<Post> posts = [
-  Post('test1','author1', 'assets/images/01.jpg'),
-  Post('test2','author2', 'assets/images/02.jpg'),
-  Post('test3','author3', 'assets/images/03.jpg')
+  Post('test1', 'author1', 'assets/images/01.jpg'),
+  Post('test2', 'author2', 'assets/images/02.jpg'),
+  Post('test3', 'author3', 'assets/images/03.jpg')
 ];
 
 /// 设置宽度
